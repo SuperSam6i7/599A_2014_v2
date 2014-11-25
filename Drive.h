6399cbd501b3359f0e73cb231039b5b43cbdba72
Samuel Fischbach <sammy.fischbach@gmail.com>
2014-11-24 16:20:51 -0800

    Added forward drive + turning

--- Drive.h
+++ Drive.h
@@ -9,3 +9,37 @@ void moveRightMotors(int speed)
 	motor[right_front] = speed;
 	motor[right_rear] = speed;
 }
+
+void drive()
+{
+	if (vexRt[Ch1] > 0)
+	{
+		moveLeftMotors(vexRt[Ch1]);
+		moveRightMotors(vexRT[Ch1]);
+	}
+	if (vexRT[Ch1] == 0)
+	{
+		moveLeftMotors(0);
+		moveRightMotors(0);
+	}
+	if (vexRT[Ch1] < 0)
+	{
+		moveLeftMotors(vexRT[Ch1]);
+		moveRightMotors(vexRT[Ch1]);
+	}
+	if (vexRT[Ch2] > 0)
+	{
+		moveLeftMotors(127);
+		moveRightMotors(-127);
+	}
+	if (vexRT[Ch2] == 0)
+	{
+		moveLeftMotors(0);
+		moveRightMotors(0);
+	}
+	if (vexRT[Ch2] < 0)
+	{
+		moveLeftMotors(-127);
+		moveRightMotors(127);
+	}
+}
